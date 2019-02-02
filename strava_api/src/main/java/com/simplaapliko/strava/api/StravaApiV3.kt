f761b6a91cd4fe0214bbd10efcd0f66d22120ff3
Oleg Kan <oleg.kan@hotmail.com>
2019-02-02 16:56:26 -0500

    api: fix AUTHORIZE_URL

--- strava_api/src/main/java/com/simplaapliko/strava/api/StravaApiV3.kt
+++ strava_api/src/main/java/com/simplaapliko/strava/api/StravaApiV3.kt
@@ -20,6 +20,6 @@ object StravaApiV3 {
     const val BASE_URL = "https://www.strava.com/api/v3/"
 
     const val BASE_OAUTH_URL = "https://www.strava.com/oauth/"
-    const val AUTHORIZE_URL = "$BASE_OAUTH_URL/mobile/authorize"
+    const val AUTHORIZE_URL = "${BASE_OAUTH_URL}mobile/authorize"
 }
 

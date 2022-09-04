25329fd2d257ef044c6d50cd627845e5da2568b1
Oleg Kan <oleg.kan@hotmail.com>
2022-09-04 16:44:35 -0700

    add DistanceType

---
+++ strava_api/src/main/kotlin/com/simplaapliko/strava/model/DistanceType.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2018 Oleg Kan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.simplaapliko.strava.model
+
+/**
+ * A list of known distances.
+ * Note: not from official documentation.
+ */
+enum class DistanceType(val id: String) {
+    METER_400("400m"),
+    KM_1("1k"),
+    KM_5("5k"),
+    KM_10("10k"),
+    KM_15("15k"),
+    KM_20("20k"),
+    KM_30("30k"),
+    MILE_HALF("1/2 mile"),
+    MILE_1("1 mile"),
+    MILE_2("2 mile"),
+    MILE_10("10 mile"),
+    MARATHON_HALF("Half-Marathon"),
+    MARATHON("Marathon")
+}
